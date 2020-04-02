ba05cd69943ec23432b039bd337939305c3dd314
Emil Linder <emil.linder1@learnet.se>
2020-04-02 15:39:05 +0200

    övat på programmet tiled

---
+++ javascript/tiled/tiled.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="tilesheet" tilewidth="32" tileheight="32" tilecount="32" columns="8">
+ <image source="tilesheet.png" width="256" height="128"/>
+</tileset>
