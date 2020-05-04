c6ab985963c68c174625086bbbdee18bc8afb2ce
tyrtor <tyrtor2@gmail.com>
2020-05-04 11:06:05 +0200

    övat på api, lägga ut marker på karta

--- api/trafiklabbet/hallplatser.js
+++ api/trafiklabbet/hallplatser.js
@@ -7,10 +7,10 @@ var lonNTI = 18.048323;
 
 /* skapa karta */
 var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v9',
-    center: [lonNTI, latNTI],
-    zoom: 10
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v9',
+  center: [lonNTI, latNTI],
+  zoom: 10
 });
 
 getLocation();
@@ -24,30 +24,51 @@ function getLocation() {
 }
 
 function showPosition(position) {
-    var latHem = position.coords.latitude;
-    var lonHem = position.coords.longitude;
+  var latHem = position.coords.latitude;
+  var lonHem = position.coords.longitude;
   console.log("Latitude: " + latHem + "<br>Longitude: " + lonHem);
 
   /* infoga en marker på kartan */
   var marker = new mapboxgl.Marker()
-        .setLngLat([lonHem, latHem])
+    .setLngLat([lonHem, latHem])
+    .addTo(map);
+
+  /* skicka lat och lon till php */
+  var ajax = new XMLHttpRequest();
+
+  /* Omvandla data till POST */
+  var postData = new FormData();
+  postData.append("lat", latHem);
+  postData.append("lon", lonHem);
+
+  /* skicka data */
+  ajax.open("POST", "./trafiklab.php")
+  ajax.send(postData);
+
+  /* ta emot svaret */
+  ajax.addEventListener("loadend", function () {
+    //console.log(this.responseText);
+
+    var hållplatser = JSON.parse(this.responseText);
+
+    hållplatser.forEach(function (hållplats) {
+
+      console.log(hållplats.name, hållplats.lat, hållplats.lon);
+
+      /* Skapa en special markerikon för skolan */
+      var buss = document.createElement('div');
+      buss.className = 'buss';
+
+      /* Skapa en popup med text */
+      var popup = new mapboxgl.Popup({ offset: 25 })
+        .setText(hållplats.name);
+
+      /* Infoga specialmarkern */
+      new mapboxgl.Marker(buss)
+        .setLngLat([hållplats.lon, hållplats.lat])
+        .setPopup(popup)
         .addTo(map);
 
-    /* skicka lat och lon till php */
-    var ajax = new XMLHttpRequest();
-
-    /* Omvandla data till POST */
-    var postData = new FormData();
-    postData.append("lat", latHem);
-    postData.append("lon", lonHem);
-
-    /* skicka data */
-    ajax.open("POST", "./trafiklab.php")
-    ajax.send(postData);
-
-    /* ta emot svaret */
-    ajax.addEventListener("loadend", function () {
-        console.log("Tar emot svar: ", this.responseText);
-        
-    })
+    });
+  });
 }
--- api/trafiklabbet/trafiklab.php
+++ api/trafiklabbet/trafiklab.php
@@ -9,6 +9,7 @@
 $lat = filter_input(INPUT_POST, "lat", FILTER_SANITIZE_STRING);
 $lon = filter_input(INPUT_POST, "lon", FILTER_SANITIZE_STRING);
 
+
 if ($lat && $lon) {
     /* api nyckel */
 $api = "5a04359da47042b7837f88a5c61908c9";
@@ -28,9 +29,15 @@ $json = file_get_contents($url);
 /* avkoda json */
 $jsonData = json_decode($json);
 
-var_dump($jsonData);
+/* plocka ut hållplatser */
+$stopLocation = $jsonData->LocationList->StopLocation;
+
+/* skicka tillbaka arrayen */
+echo json_encode($stopLocation);
+
+echo $json;
 } else {
-    echo"<p>Nåhot gick fel med in data</p>";
+    echo"<p>Något gick fel med in data</p>";
 }
 ?>
 
